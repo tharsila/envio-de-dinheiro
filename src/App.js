75cb293b6076b224dd4a64e650a0a226c8931e0d
tharsila <tharsilapaula@gmail.com>
2022-06-23 17:57:20 -0300

    add página de listagem de usuários

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const url = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce';
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const charge = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setUsers(data);
+    }
+    charge();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      { 
+        users.map((user, index) => (
+          <div className="user-container">
+            <div key={index} className="user-content">
+              <div className="user-info">
+                <div className="user-img">
+                  <img src={user.img} alt="imagem do usuario" />
+                </div>
+                <div className="user-data">
+                  <span>Nome do Usuário: {user.name}</span>
+                  <p><span>ID: {user.id}</span> - <span>Username: {user.username}</span></p>
+                </div>
+              </div>
+              <div className="btn">
+                <button>Pagar</button>
+              </div>
+            </div> 
+          </div>
+        ))
+      }
+    </main>
   );
-}
+};
 
 export default App;
